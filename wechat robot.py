dfd3542da39b7909a87126458bc0b642958c73d7
sscpc <15301993610@163.com>
2019-12-02 21:38:43 +0800

    将查找好友和群的功能写成独立的函数，优化程序

--- wechat robot.py
+++ wechat robot.py
@@ -7,15 +7,9 @@
 from wxpy import *
 import time
 
+# ## 查找好友、群
 
-# ## 指定时间给指定人、群发送信息
-
-# In[70]:
-
-
-#date '11-24 00:00:00'
-def minput(name,date,message):
-    flag=0
+def fgsearch(name):
     found=[]
     try:
         found=bot.friends().search(name)
@@ -24,45 +18,47 @@ def minput(name,date,message):
             bot.file_helper.send('未找到好友')
         else:
              #判断搜索和查找结果是否完全匹配
+            print(found)
             for i in range(len(found)):
                 if found[i].name==name:
-                    name=found[i]
-                    break
-            if type(name)==str:
-                print('未找到好友') 
-                bot.file_helper.send('未找到好友') 
-            else:
-                print('好友已找到') 
-                bot.file_helper.send('好友已找到')
-                flag=1
+                    print('好友已精确找到') 
+                    bot.file_helper.send('好友已精确找到') 
+                    return found[i]
+            print('未精确找到好友') 
+            bot.file_helper.send('未精确找到好友')
+            flag=1
     except ResponseError as e:
         print(e.err_code, e.err_msg) # 查看错误号和错误消息
         print('好友查找出错') 
         bot.file_helper.send('好友查找出错')
-    if flag==0:
-        try:
-            found=bot.groups().search(name)
-            if not found:
-                print('未找到微信群') 
-                bot.file_helper.send('未找到微信群')
-            else:
-                 #判断搜索和查找结果是否完全匹配
-                for i in range(len(found)):
-                    if found[i].name==name:
-                        name=found[i]
-                        break
-                if type(name)==str:
-                    print('未找到微信群') 
-                    bot.file_helper.send('未找到微信群') 
-                else:
-                    print('微信群已找到') 
-                    bot.file_helper.send('微信群已找到')
-                    flag=2
-        except ResponseError as e:
-            print(e.err_code, e.err_msg) # 查看错误号和错误消息
-            print('群查找出错') 
-            bot.file_helper.send('群查找出错')
-    if flag==0:return 0
+    try:
+        found=bot.groups().search(name)
+        if not found:
+            print('未精确找到微信群') 
+            bot.file_helper.send('未精确找到微信群')
+        else:
+            print(found)
+             #判断搜索和查找结果是否完全匹配
+            for i in range(len(found)):
+                if found[i].name==name:
+                    print('微信群已精确找到') 
+                    bot.file_helper.send('微信群已精确找到')
+                    return found[i]
+            print('未找到微信群') 
+            bot.file_helper.send('未找到微信群') 
+    except ResponseError as e:
+        print(e.err_code, e.err_msg) # 查看错误号和错误消息
+        print('群查找出错') 
+        bot.file_helper.send('群查找出错')
+    return []
+
+# ## 指定时间给指定人、群发送信息
+
+# In[70]:
+
+
+#date '11-24 00:00:00'
+def minput(name,date,message):
     day=date[3:5]
     hour=date[6:8]
     minute=date[-5:-3]
@@ -79,9 +75,11 @@ def minput(name,date,message):
     bot.file_helper.send('信息已发送')
 def msend():
     name=input('请输入名字：')
+    inputname=search(name)
+    if not inputname:return 0
     date=input('请输入日期（MM-DD hh:mm:ss）：')
     message=input('请输入发送信息：')
-    minput(name,date,message)
+    minput(inputname,date,message)
 
 
 # ### 查找群成员
@@ -119,7 +117,7 @@ def gsearch(name,group):
 
 
 #记录消息类型、内容、发送人、接收时间
-def search(message):
+def newsearch(message):
     bot.messages.max_history = 1000
     meslist=[]
     mesid=[]
@@ -130,12 +128,14 @@ def search(message):
             mesid.append(mes.id)
             meslist.append([mes.type,mes.text,mes.sender,mes.member,mes.receive_time.strftime("%Y-%m-%d %H:%M:%S")])
     print(meslist)
+    bot.file_helper.send(meslist)
 def recordsearch():
     message=input('请输入查找信息：')
-    search(message)
+    newsearch(message)
 
 
 bot=Bot(cache_path=True)#console_qr=1
+#fgsearch(name)#查找好友、群
 #msend()#指定时间给指定好友、群发消息
 #gsearch(name,group)#查找群成员
 #recordsearch()#查找聊天记录(登录以后的)
